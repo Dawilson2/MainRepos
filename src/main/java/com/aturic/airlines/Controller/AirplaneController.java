c122eb0307c492328d03084609b5794999129faa
Bobby Sparks III <bobbysparksiii@Bobbys-MacBook-Pro-2.local>
2023-04-03 19:27:50 -0500

    small changes to hello world

---
+++ src/main/java/com/aturic/airlines/Controller/AirplaneController.java
@@ -0,0 +1,12 @@
+package com.aturic.airlines.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AirplaneController {
+
+    public void HelloWorld(){
+        System.out.println("Hello world!");
+
+    }
+}
